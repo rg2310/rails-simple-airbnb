d80af8b104a115c5329ac3360b6ebb4f1c87f71c
Renee Godyn <renitatje@hotmail.com>
2022-01-20 20:14:51 +0000

    added 7 CRUDE routes & defined index in the view

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
